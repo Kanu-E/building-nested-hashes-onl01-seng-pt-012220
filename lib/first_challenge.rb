972b520b8418dad53b9dd52ad64beb0c5eaac887
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-02-03 19:37:13 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,9 @@ def first_challenge(epic_tragedy)
    epic_tragedy = {
      "Montague" => {"Patriach"=> Name:"" , age: ""
    }
-     
+     "capulet" => {"Patriach"=> Name:"" , age: ""
+   }
+
    }
    
 end
