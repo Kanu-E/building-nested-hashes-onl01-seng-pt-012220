c039ef476125af78fe25d4c8270bc1f335dc0d45
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-02-03 20:04:47 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,7 +1,11 @@
 def first_challenge
   epic_tragedy = {
+     :montague => {
+      :patriarch => {name: "Lord Montague", age: "53"}, 
     },
-    {
+    :capulet => {
+        :patriarch => {name: "Lord Capulet", age: "50"},
+    }
    }
 end
 
